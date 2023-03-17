3421c1603f7df69e7770c9d996e7fbb8bc60dec5
niquejoe <nikie.deocampo@gmail.com>
2023-03-17 09:55:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
-
+#Do this only once
 import pathlib
-data_dir = "flower_photos"
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
 data_dir = pathlib.Path(data_dir)
 
 
